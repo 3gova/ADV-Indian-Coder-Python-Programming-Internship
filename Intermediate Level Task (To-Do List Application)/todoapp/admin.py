e29ad4a6452613a589b2b217b3365cea546f5a8b
Govarthanan.J <105597799+3gova@users.noreply.github.com>
2023-12-07 14:26:06 +0530

    Delete Intermediate Level Task (To-Do List Application) directory

--- Intermediate Level Task (To-Do List Application)/todoapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from todoapp.models import todo
-
-# Register your models here.
-
-class todoadmin(admin.ModelAdmin):
-    list = ['sno','task_name','status']
-admin.site.register(todo,todoadmin)
